2ac3bf878d9fffa0acb8fcb30cb690ea5a86284c
Juliano Pereira <jpereira@dataweb.com.br>
2024-12-19 11:47:57 -0300

    Adicionar arquivos de projeto.

---
+++ Db/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db
+{
+    internal class Task
+    {
+        public int id { get; set; }
+        public required string nome { get; set; }
+        public required string categoria { get; set; }
+        public int userId { get; set; }
+
+    }
+}
